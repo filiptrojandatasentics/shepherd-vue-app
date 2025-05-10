8e9317b07326d517d08630afc08055e771d82420
filip <f.trojan@centrum.cz>
2025-05-10 08:41:35 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import Button from "primevue/button";
+import Avatar from "primevue/avatar";
+import Toolbar from 'primevue/toolbar';
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <Toolbar style="border-radius: 2rem; padding: 1rem 1rem 1rem 1.5rem">
+    <template #start>
+        <div class="flex items-center gap-2">
+            <Button label="Hello" text plain />
+            <Button label="Budgets" text plain />
+            <Button label="Projects" text plain />
+            <Button label="POs" text plain />
+            <Button label="Invoices" text plain />
+            <Button label="Customers" text plain />
+            <Button label="People" text plain />
+            <Button label="Preferences" text plain />
+            <Button label="Tags" text plain />
+        </div>
+    </template>
+    <template #end>
+        <div class="flex items-center gap-2">
+            <Avatar image="src/assets/Filip-Trojan-320-320.png" style="width: 32px; height: 32px" />
+        </div>
+    </template>
+    </Toolbar>
+  </div>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Ahoj Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref } from 'vue'
+import Button from "primevue/button";
+import DatePicker from 'primevue/datepicker';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0);
+const date = ref(null);
+const products = [
+  {
+    code: 'aaa',
+    name: 'nnnnnn',
+    category: 'c1',
+    quantity: 12,
+  },
+  {
+    code: 'bbb',
+    name: 'nnnnnn',
+    category: 'c2',
+    quantity: 33,
+  },
+];
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <Button type="button" @click="count++">count is {{ count }}</Button>
+  </div>
+  <div class="card">
+    <DatePicker v-model="date" dateFormat="yy-mm-dd"/>
+    <p>{{ date }}</p>
+  </div>
+  <div class="card">
+    <DataTable :value="products" tableStyle="min-width: 50rem">
+      <Column field="code" header="Code"></Column>
+      <Column field="name" header="Name"></Column>
+      <Column field="category" header="Category"></Column>
+      <Column field="quantity" header="Quantity"></Column>
+    </DataTable>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Learn more about IDE Support for Vue in the
+    <a
+      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      target="_blank"
+      >Vue Docs Scaling up Guide</a
+    >.
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
