8c7cc1973624b2c308a33897a3e07af7784845aa
filip <f.trojan@centrum.cz>
2025-05-10 10:34:26 +0200

    Splitter

--- src/components/BudgetMain.vue
+++ src/components/BudgetMain.vue
@@ -1,3 +1,17 @@
+<script setup>
+import Splitter from 'primevue/splitter';
+import SplitterPanel from 'primevue/splitterpanel';
+</script>
+
 <template>
-    <h2>Budget Main</h2>
+    <Splitter style="height: 500px" layout="vertical">
+        <SplitterPanel class="flex items-center justify-center">
+            <Splitter layout="horizontal">
+                <SplitterPanel class="flex items-center justify-center" :size="30" :minSize="10"> Panel 1.1 </SplitterPanel>
+                <SplitterPanel class="flex items-center justify-center" :size="30"> Panel 1.2 </SplitterPanel>
+                <SplitterPanel class="flex items-center justify-center" :size="30"> Panel 1.3 </SplitterPanel>
+            </Splitter>
+        </SplitterPanel>
+        <SplitterPanel class="flex items-center justify-center"> Panel 2 </SplitterPanel>
+    </Splitter>
 </template>
