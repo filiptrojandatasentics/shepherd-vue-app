611b3d21e409183a2cb5d19a6677a3a2be3607df
filip <f.trojan@centrum.cz>
2025-05-17 10:06:50 +0200

    total_work InputNumber

--- src/components/ProjectPlanning.vue
+++ src/components/ProjectPlanning.vue
@@ -1,9 +1,26 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import Fieldset from 'primevue/fieldset';
+import { InputNumber } from "primevue";
+
+const project = ref({
+    config: {
+        work_step: 8
+    },
+    total: {
+        work: 100*8,
+        time: 5*30*20,
+    }
+});
+
+onMounted(() => {
+});
 </script>
 
 <template>
     <Fieldset legend="total">
+            <InputNumber v-model="project.total.work" showButtons :step="project.config.work_step" />
+            <InputNumber v-model="project.config.work_step" />
         <p class="m-0">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
