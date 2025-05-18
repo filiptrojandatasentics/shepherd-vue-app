dc7221e60b76af8c8a9cf0d7f82044837e79ab19
filip <f.trojan@centrum.cz>
2025-05-18 15:09:03 +0200

    add DataTable

--- src/components/ProjectPlanning.vue
+++ src/components/ProjectPlanning.vue
@@ -2,14 +2,49 @@
 import { ref, onMounted } from "vue";
 import Fieldset from 'primevue/fieldset';
 import { InputNumber } from "primevue";
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+const users = [
+    'jiri.jelinek@datasentics.com',
+    'filip.trojan@datasentics.com',
+    'david.vopelka@datasentics.com',
+    'tomas.bouma@datasentics.com',
+    'ondrej.kral@datasentics.com',
+    'ondrej.pleticha@datasentics.com',
+];
 
 const project = ref({
     config: {
-        work_step: 8
+        work_step: 1,
+        work_unit: "md",
     },
     total: {
-        work: 100*8,
+        work: 100,
         time: 5*30*20,
+    },
+    people: {
+        list: [
+            {
+                name: "filip.trojan",
+                fte: 0.3,
+                rate: 900,
+                work: 20
+            },
+            {
+                name: 'robin.siroky',
+                fte: 0.8,
+                rate: 800,
+                work: 80
+            }
+        ],
+        selected_person: null,
+        person: {
+            name: null,
+            fte: null,
+            rate: null,
+            work: null
+        }
     }
 });
 
@@ -27,10 +62,19 @@ onMounted(() => {
         </p>
     </Fieldset>
     <Fieldset legend="people">
-        <p class="m-0">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <div class="card">
+            <DataTable 
+                :value="project.people.list" 
+                selectionMode="single"
+                dataKey="name"
+                v-model:selection="project.people.selected_person"
+                tableStyle="min-width: 50rem">
+                <Column field="name" header="name"></Column>
+                <Column field="fte" header="fte"></Column>
+                <Column field="rate" header="rate"></Column>
+                <Column field="work" header="work"></Column>
+            </DataTable>
+        </div>
     </Fieldset>
     <Fieldset legend="time">
         <p class="m-0">
